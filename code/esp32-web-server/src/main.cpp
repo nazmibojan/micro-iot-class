41a5c5bf85c779f27739bd7feb0dabcbcf0b5c84
Nazmi Febrian <nazmi.febrian@gmail.com>
2020-07-19 10:37:06 +0700

    Add web server code

--- code/esp32-web-server/src/main.cpp
+++ code/esp32-web-server/src/main.cpp
@@ -3,114 +3,139 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <DHT.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncTCP.h>
 
 #define DHT_PIN 23
 #define DHT_TYPE DHT11
 
 float temperature = 0, humidity = 0;
 String tempString, humidString;
-const char *ssid = "Mako Brimob";
-const char *password = "mantapjiwa";
+long lastPublish = 0, lastConnected = 0;
+const char *ssid = "ipop";
+const char *password = "iyanfopi";
+String header;
+String ledState = "off";
 
 DHT dht(DHT_PIN, DHT_TYPE);
-AsyncWebServer server(80);
+WiFiServer server(80);
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP32 DHT Server</h2>
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">%</sup>
-  </p>
-</body>
-<script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 10000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 10000 ) ;
-</script>
-</html>)rawliteral";
-
-String processor(const String& var);
 void updateDhtData();
 void connectToNetwork();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
   dht.begin();
   connectToNetwork();
 
   Serial.println(WiFi.macAddress());
   Serial.println(WiFi.localIP());
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/plain", tempString.c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/plain", humidString.c_str());
-  });
-
   // Start server
   server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  updateDhtData();
-  delay(5000);
+  WiFiClient client = server.available();
+
+  if (client) {
+    while(client.available()) {
+    String request = client.readStringUntil('\n');
+    header += request;
+    Serial.print(request);
+    Serial.println(request.length());
+
+    // lastConnected = millis();
+    // String currentLine = "";
+
+    // while (client.connected() && millis() - lastConnected <= 2000) {
+    //   // Read client data
+    //   if (client.available()) {
+    //     char c = client.read();
+    //     Serial.print(c);
+    //     header += c;
+
+    //     if (c == '\n') {
+    //       if (currentLine.length() == 0) {
+      if (request.length() - 1 == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            if (header.indexOf("GET /led/on") >= 0) {
+              Serial.println("LED on");
+              ledState = "on";
+              digitalWrite(BUILTIN_LED, HIGH);
+            } else if (header.indexOf("GET /led/off") >= 0) {
+              Serial.println("LED off");
+              ledState = "off";
+              digitalWrite(BUILTIN_LED, LOW);
+            }
+            header = "";
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta http-equiv=\"refresh\" content=\"30\", "
+                           "name=\"viewport\" "
+                           "content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>body { text-align: center; font-family: "
+                           "\"Arial\", Arial;}");
+            client.println(
+                "table { border-collapse: collapse; width:40%; "
+                "margin-left:auto; margin-right:auto; border-spacing: "
+                "2px; background-color: white; border: 4px solid green; }");
+            client.println("th { padding: 20px; background-color: #008000; "
+                           "color: white; }");
+            client.println("tr { border: 5px solid green; padding: 2px; }");
+            client.println("tr:hover { background-color:yellow; }");
+            client.println("td { border:4px; padding: 12px; }");
+            client.println(
+                ".sensor { color:red; font-weight: bold; padding: 1px;}");
+            client.println(
+                ".button { background-color: #4CAF50; border: none; color: "
+                "white; padding: 16px 40px; }</style>");
+            client.println(
+                "</head><body><h1>ESP32 Web Server Reading sensor values</h1>");
+            client.println("<h2>DHT11</h2>");
+            client.println(
+                "<table><tr><th>MEASUREMENT</th><th>VALUE</th></tr>");
+            client.println(
+                "<tr><td>Temp. Celsius</td><td><span class=\"sensor\">");
+            client.println(temperature);
+            client.println(" *C</span></td></tr>");
+            client.println(
+                "<tr><td>Temp. Fahrenheit</td><td><span class=\"sensor\">");
+            client.println(1.8 * temperature + 32);
+            client.println(" *F</span></td></tr>");
+            client.println("<tr><td>Humidity</td><td><span class=\"sensor\">");
+            client.println(humidity);
+            client.println(" %</span></td></tr></table>");
+            if (ledState == "on") {
+              client.println("<p><a href=\"/led/off\"><button "
+                             "class=\"button\">OFF</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/led/on\"><button "
+                             "class=\"button\">ON</button></a></p>");
+            }
+            client.println("</body></html>");
+            client.println();
+            client.stop();
+    //       } else {
+    //         currentLine = "";
+    //       }
+    //     } else if (c != '\r') {
+    //       currentLine += c;
+    //     }
+      }
+    }
+  }
+
+  if (millis() - lastPublish > 5000) {
+    updateDhtData();
+    lastPublish = millis();
+  }
 }
 
 void connectToNetwork() {
@@ -124,18 +149,6 @@ void connectToNetwork() {
   Serial.println("Connected to network");
 }
 
-// Replaces placeholder with DHT values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "TEMPERATURE"){
-    return tempString;
-  }
-  else if(var == "HUMIDITY"){
-    return humidString;
-  }
-  return String();
-}
-
 void updateDhtData() {
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
@@ -143,5 +156,6 @@ void updateDhtData() {
   tempString = String(temperature);
   humidString = String(humidity);
 
-  Serial.println("Temperature: " + tempString + " C & Humidity: " + humidString + " %");
+  Serial.println("Temperature: " + tempString + " C & Humidity: " +
+                 humidString + " %");
 }
