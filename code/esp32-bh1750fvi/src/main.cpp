feaa9ae8810ab84678e76c39d945799d50095bc3
Nazmi Febrian <nazmi.febrian@pitik.id>
2023-11-17 13:54:53 +0700

    remove begin transmission when reading light

--- code/esp32-bh1750fvi/src/main.cpp
+++ code/esp32-bh1750fvi/src/main.cpp
@@ -1,3 +1,6 @@
+// #define USE_BH1750_LIB
+
+#ifndef USE_BH1750_LIB
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -9,70 +12,78 @@ int bh1750Read(int address);
 
 byte buff[2];
 unsigned short lux = 0;
-int x = 0;
-float luas1, luas2, luas3;
-
-float hitungVolumeTabung(int radian, int height);
 
 void setup() {
-  Wire.begin();
-  Serial.begin(9600);
+    Serial.begin(9600);
+    Wire.begin();
+    ESP_LOGI("SETUP", "Start Reading Light Sensor..");
 }
 
 void loop() {
-  bh1750Request(BH175O_ADDRESS);
-  delay(200);
+    bh1750Request(BH175O_ADDRESS);
+    delay(200);
 
-  if (bh1750Read(BH175O_ADDRESS) == BHI1750_DATALEN) {
-    lux = (((unsigned short)buff[0] << 8) | (unsigned short)buff[1]) / 1.2;
-    Serial.print(lux);
-    Serial.println("lux");
-  }
-  delay(1000);
-
-  function2();
-
-  luas1 = hitungVolumeTabung(4, 5);
-  luas2 = hitungVolumeTabung(4, 6);
+    if (bh1750Read(BH175O_ADDRESS) == BHI1750_DATALEN) {
+        lux = (((unsigned short)buff[0] << 8) | (unsigned short)buff[1]) / 1.2;
+        ESP_LOGI("LIGHT", "Light Intensity: %d", lux);
+    } else {
+        ESP_LOGE("LIGHT", "Error reading bh1750 sensor");
+    }
+    delay(1000);
 }
 
-int bh1750Read(int address) //
+int bh1750Read(int address)  //
 {
-  int i = 0;
+    if (Wire.requestFrom((int)address, (int)2) == 2) {
+        buff[0] = Wire.read();
+        buff[1] = Wire.read();
 
-  Wire.beginTransmission(address);
-  Wire.requestFrom(address, 2);
-  while (Wire.available()) //
-  {
-    buff[i] = Wire.read(); // receive one byte
-    i++;
-  }
-  Wire.endTransmission();
-
-  return i;
+        return 2;
+    } else {
+        return 0;
+    }
 }
 
 void bh1750Request(int address) {
-  Wire.beginTransmission(address);
-  Wire.write(0x10);                 // 1lx reolution 120ms
-  Wire.endTransmission();
+    Wire.beginTransmission(address);
+    Wire.write(0x10);  // 1lx resolution 120ms
+
+    if (Wire.endTransmission() == 0) {
+        ESP_LOGI("BH1750", "Success end transmission");
+    } else {
+        ESP_LOGE("BH1750", "Failed end transmission");
+    }
 }
 
+#else
+/*
+  Example of BH1750 library usage. This example initialises the BH1750 object using the default high resolution continuous mode and then makes a light level reading every second.
+*/
+#include <Arduino.h>
+#include <BH1750.h>
+#include <Wire.h>
 
-void function1(int x, int y) {
+BH1750 lightMeter;
 
+void setup() {
+    Serial.begin(9600);
+
+    // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+    Wire.begin();
+    // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
+    // For Wemos / Lolin D1 Mini Pro and the Ambient Light shield use Wire.begin(D2, D1);
+
+    lightMeter.begin();
+
+    Serial.println(F("BH1750 Test begin"));
 }
 
-void function2() {
-  x = 1;
+void loop() {
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+    delay(1000);
 }
 
-String hitungVolumeTabung(int radian, int height) {
-  float pi = 3.14;
-  float luasFungsi;
-  String output;
-
-  luasFungsi = pi * radian * radian * height;
-
-  return luasFungsi;
-}
+#endif
--- code/esp32-preferences/src/main.cpp
+++ code/esp32-preferences/src/main.cpp
@@ -1,48 +1,72 @@
 #include <Arduino.h>
 #include <Preferences.h>
 
-#define EEPROM_SIZE 1
+#define STORAGE_NAME "storage"
+#define LED_STATUS_KEY "led"
+#define SSID_KEY "ssid"
 #define BUTTON_PIN 21
 
-unsigned char ledStatus = LOW;
+bool ledStatus = LOW;
 bool changeLedStatus = false;
 portMUX_TYPE gpioIntMux = portMUX_INITIALIZER_UNLOCKED;
 
 Preferences storage;
+String ssid;
+uint32_t dataIndex = 0;
 
 void IRAM_ATTR gpioISR() {
-  portENTER_CRITICAL(&gpioIntMux);
-  changeLedStatus = true;
-  portEXIT_CRITICAL(&gpioIntMux);
+    portENTER_CRITICAL(&gpioIntMux);
+    changeLedStatus = true;
+    portEXIT_CRITICAL(&gpioIntMux);
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(BUILTIN_LED, OUTPUT);
-  attachInterrupt(BUTTON_PIN, &gpioISR, FALLING);
+    // put your setup code here, to run once:
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(BUILTIN_LED, OUTPUT);
+    attachInterrupt(BUTTON_PIN, &gpioISR, FALLING);
 
-  // Initialize preferences and read LED status
-  storage.begin("Storage", true);
-  delay(100);
-  ledStatus = storage.getUChar("led_state");
-  digitalWrite(BUILTIN_LED, ledStatus);
-  storage.end();
+    Serial.begin(9600);
+
+    // Initialize Preferences and read LED status
+    storage.begin(STORAGE_NAME);
+    delay(100);
+    ledStatus = storage.getBool(LED_STATUS_KEY);
+    digitalWrite(BUILTIN_LED, ledStatus);
+
+    // Print current SSID
+    ssid = storage.getString(SSID_KEY);
+    ESP_LOGI("SETUP", "SSID name: %s", ssid);
+    ssid.clear();
+    storage.end();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (changeLedStatus) {
-    portENTER_CRITICAL(&gpioIntMux);
-    changeLedStatus = false;
-    portEXIT_CRITICAL(&gpioIntMux);
+    // put your main code here, to run repeatedly:
+    if (changeLedStatus) {
+        portENTER_CRITICAL(&gpioIntMux);
+        changeLedStatus = false;
+        portEXIT_CRITICAL(&gpioIntMux);
 
-    ledStatus = !ledStatus;
-    digitalWrite(BUILTIN_LED, ledStatus);
+        ledStatus = !ledStatus;
+        digitalWrite(BUILTIN_LED, ledStatus);
 
-    storage.begin("Storage", true);
-    storage.putUChar("led_state", ledStatus);
-    storage.end();
-  }
+        storage.begin(STORAGE_NAME);
+        storage.putBool(LED_STATUS_KEY, ledStatus);
+        storage.end();
+    }
+
+    // put string data
+    if (Serial.available()) {
+        ssid[dataIndex] = Serial.read();
+        dataIndex++;
+
+        if (ssid[dataIndex - 1] == '\n') {
+            dataIndex = 0;
+            storage.begin(STORAGE_NAME);
+            storage.putString(SSID_KEY, ssid);
+            ssid.clear();
+            storage.end();
+        }
+    }
 }
-
