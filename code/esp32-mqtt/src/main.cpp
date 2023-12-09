269e90539a35be7b424ca39278358e7df0771e18
Nazmi Febrian <nazmi.febrian@pitik.id>
2023-12-09 08:04:42 +0700

    change mqtt subscribe code

--- code/esp32-mqtt/src/main.cpp
+++ code/esp32-mqtt/src/main.cpp
@@ -7,7 +7,7 @@
 #define DHT_PIN 4
 #define DHT_TYPE DHT11
 
-#define PUBLISH_INTERVAL 10000
+#define PUBLISH_INTERVAL 30000
 
 unsigned long lastPublish = 0;
 float tempFloat, humidFloat;
@@ -16,8 +16,8 @@ String humidity;
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
-String ssid = "Praktisi AP";
-String pass = "semangka";
+String ssid = "NZM IoT Lab";
+String pass = "Heisenberg1932";
 
 String mqttServer = "broker.hivemq.com";
 String mqttUser = "";
@@ -26,7 +26,7 @@ String mqttPort = "1883";
 
 String deviceId = "Home_Gateway_1";
 String pubTopic = String(deviceId + "/sensor_data_nazmi");
-// String subTopic = String(deviceId + "/led_status");
+String subTopic = String(deviceId + "/led_status_nazmi");
 
 WiFiClient ESPClient;
 PubSubClient ESPMqtt(ESPClient);
@@ -35,8 +35,8 @@ void connectToNetwork();
 void connectToMqtt();
 void publishMessage();
 void updateDhtData();
-// void mqttCallback(char *topic, byte *payload, long length);
-// void do_actions(const char *message);
+void mqttCallback(char *topic, byte *payload, long length);
+void do_actions(const char *message);
 
 void setup() {
     Serial.begin(9600);
@@ -47,7 +47,7 @@ void setup() {
     // Disconnecting previous WiFi
     WiFi.disconnect();
     ESPMqtt.setServer(mqttServer.c_str(), mqttPort.toInt());
-    // ESPMqtt.setCallback(mqttCallback);
+    ESPMqtt.setCallback(mqttCallback);
 
     connectToNetwork();
     connectToMqtt();
@@ -89,7 +89,7 @@ void connectToMqtt() {
         if (ESPMqtt.connect("ESP32Client-NazmiFeb", mqttUser.c_str(), mqttPwd.c_str())) {
             ESP_LOGI("MQTT", "Connected to Server");
             // subscribe to the topic
-            // ESPMqtt.subscribe(subTopic.c_str());
+            ESPMqtt.subscribe(subTopic.c_str());
         } else {
             ESP_LOGI("MQTT", "ERROR > failed with state %d", ESPMqtt.state());
             delay(2000);
@@ -124,32 +124,49 @@ void updateDhtData() {
     humidFloat = dht.readHumidity();
 }
 
-// void mqttCallback(char *topic, byte *payload, long length) {
-//     char msg[256];
+void mqttCallback(char *topic, byte *payload, long length) {
+    char msg[256] = "";
 
-//     ESP_LOGI("MQTT", "Message arrived [%s]", subTopic);
-//     for (int i = 0; i < length; i++) {
-//         msg[i] = (char)payload[i];
-//     }
-//     do_actions(msg);
-// }
+    ESP_LOGI("MQTT", "Message arrived [%s]", subTopic.c_str());
+    ESP_LOGI("MQTT", "Message length [%d]", length);
+    for (int i = 0; i < length; i++) {
+        msg[i] = (char)payload[i];
 
-// void do_actions(const char *message) {
-//     const size_t capacity = JSON_OBJECT_SIZE(2) + 30;
-//     DynamicJsonDocument doc(capacity);
+        if (msg[i] == '}') {
+            break;
+        }
+    }
+    do_actions(msg);
+}
 
-//     deserializeJson(doc, message);
+void do_actions(const char *message) {
+    StaticJsonDocument<96> doc;
 
-//     const char *deviceId = doc["deviceId"];
-//     const char *ledStatus = doc["ledStatus"];
+    Serial.println(message);
 
-//     if (String(ledStatus) == "ON") {
-//         ESP_LOGI("LED", "TURN ON LED");
-//         digitalWrite(BUILTIN_LED, HIGH);
-//     } else if (String(ledStatus) == "OFF") {
-//         ESP_LOGI("LED", "TURN Off LED");
-//         digitalWrite(BUILTIN_LED, LOW);
-//     } else {
-//         ESP_LOGI("LED", "Could not recognize LED Status");
-//     }
-// }
+    DeserializationError error = deserializeJson(doc, message);
+
+    if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return;
+    }
+
+    const char *deviceId = doc["deviceId"];    // "Home_Gateway_1"
+    const char *ledStatus = doc["ledStatus"];  // "ON"
+
+    // ESP_LOGI("MQTT", "Device ID: %s", deviceId);
+    // ESP_LOGI("MQTT", "Led Status: %s", ledStatus);
+    Serial.println(deviceId);
+    Serial.println(ledStatus);
+
+    if (String(ledStatus) == "ON") {
+        ESP_LOGI("LED", "TURN ON LED");
+        digitalWrite(BUILTIN_LED, HIGH);
+    } else if (String(ledStatus) == "OFF") {
+        ESP_LOGI("LED", "TURN Off LED");
+        digitalWrite(BUILTIN_LED, LOW);
+    } else {
+        ESP_LOGI("LED", "Could not recognize LED Status");
+    }
+}
